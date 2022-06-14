75d4fa5316585277d9d744c06fe8c98b8f30118c
LlumnicaDG <argjend.llumnica@digitizeict.com>
2022-06-14 08:51:08 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App d-flex">
       <BrowserRouter>
         <Nav />
         <Routes>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,30 +1,32 @@
 import React from "react";
-import {Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function Nav() {
   return (
-    <ul>
-      <li>
-        <NavLink
-          className="navButton"
-          activeclassname="activeLink"
-          tag={Link}
-          to="/"
-        >
-          Complete Tree
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          className="navButton"
-          activeclassname="activeLink"
-          tag={Link}
-          to="/second"
-        >
-          Axios Call Tree
-        </NavLink>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <NavLink
+            className="navButton"
+            activeclassname="activeLink"
+            tag={Link}
+            to="/"
+          >
+            Complete Tree
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="navButton"
+            activeclassname="activeLink"
+            tag={Link}
+            to="/second"
+          >
+            Axios Call Tree
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 };
 
